65088f554060ab8f0fec07d7513e953f629e9456
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-08-11 02:09:59 +0200

    assets added from hero section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuBarComponent } from '../shared/nav/menu-bar/menu-bar.component';
+import { HeroComponent } from './hero/hero.component';  
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss'],
-  imports: [MenuBarComponent] // (hier später deine Sektionen rein)
+  imports: [MenuBarComponent, HeroComponent] // (hier später deine Sektionen rein)
 })
 export class MainContentComponent {}
