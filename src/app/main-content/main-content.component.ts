60d4d9044caa3637aca92937ae8e030d201785de
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-08-25 23:48:52 +0200

    beginn my-skill-component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';  
 import { WhyMeComponent } from './why-me/why-me.component';
+import{SkillsComponent} from './my-skills/skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss'],
-  imports: [ CommonModule, HeroComponent, WhyMeComponent] // (hier später deine Sektionen rein)
+  imports: [ CommonModule, HeroComponent, WhyMeComponent, SkillsComponent] // (hier später deine Sektionen rein)
 })
 export class MainContentComponent {}
