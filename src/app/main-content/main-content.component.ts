f846a133d4a32eef780f28565f51d9823dcbe3c9
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-08-31 22:20:46 +0200

    beginning my-projects-sektion but not finished

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';  
 import { WhyMeComponent } from './why-me/why-me.component';
 import{SkillsComponent} from './my-skills/skills.component';
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss'],
-  imports: [ CommonModule, HeroComponent, WhyMeComponent, SkillsComponent] // (hier später deine Sektionen rein)
+  imports: [CommonModule, HeroComponent, WhyMeComponent, SkillsComponent, ProjectsComponent] // (hier später deine Sektionen rein)
+ // (hier später deine Sektionen rein)
 })
 export class MainContentComponent {}
--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  active = 0; 
 
 }
