f15d52d1159141ebb4a2eb6e5681fb0a39d28056
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-09-07 23:16:58 +0200

    my-project-component finished

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss',
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
   active = 0; 
