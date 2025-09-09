495d3643e9179897f9f6a1c3f9d48b4ed2bb2847
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-09-09 23:12:33 +0200

    contact-section isn't finish

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,12 +5,15 @@ import { WhyMeComponent } from './why-me/why-me.component';
 import{SkillsComponent} from './my-skills/skills.component';
 import { ProjectsComponent } from "./projects/projects.component";
 import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component'; 
+
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss'],
-  imports: [CommonModule, HeroComponent, WhyMeComponent, SkillsComponent, ProjectsComponent, ReferencesComponent] // (hier später deine Sektionen rein)
+  imports: [CommonModule, HeroComponent, WhyMeComponent, SkillsComponent, ProjectsComponent, 
+    ReferencesComponent, ContactComponent] // (hier später deine Sektionen rein)
 })
 export class MainContentComponent {}
