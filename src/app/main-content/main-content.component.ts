74c489081848faceb19b7ccaf4080cca1c19e441
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-09-09 22:34:46 +0200

    references-section beginning

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,13 +4,13 @@ import { HeroComponent } from './hero/hero.component';
 import { WhyMeComponent } from './why-me/why-me.component';
 import{SkillsComponent} from './my-skills/skills.component';
 import { ProjectsComponent } from "./projects/projects.component";
+import { ReferencesComponent } from './references/references.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.scss'],
-  imports: [CommonModule, HeroComponent, WhyMeComponent, SkillsComponent, ProjectsComponent] // (hier später deine Sektionen rein)
- // (hier später deine Sektionen rein)
+  imports: [CommonModule, HeroComponent, WhyMeComponent, SkillsComponent, ProjectsComponent, ReferencesComponent] // (hier später deine Sektionen rein)
 })
 export class MainContentComponent {}
